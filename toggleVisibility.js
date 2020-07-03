1208b93238cdc76b3bb6ea6ee2586090f4d67316
skylar <qvalador@gmail.com>
2020-07-02 23:09:00 -0400

    update to version 2.0

--- toggleVisibility.js
+++ toggleVisibility.js
@@ -1,16 +1,16 @@
 function reveal() {
-    var bottom = document.getElementById("bottom");
+    var bottom = document.getElementById("drawer");
     bottom.classList.toggle("revealed");
 };
 
 window.addEventListener("scroll", function() {
-    var bottom = document.getElementById("bottom");
+    var bottom = document.getElementById("drawer");
     if (bottom.classList.contains("revealed"))
         bottom.classList.remove("revealed");
 });
 
 window.addEventListener("click", function(event) {
-    var bottom = document.getElementById("bottom");
+    var bottom = document.getElementById("drawer");
     var toggler = document.getElementById("toggler");
     if (bottom.classList.contains("revealed")) {
         if (event.target != bottom && event.target != toggler)
